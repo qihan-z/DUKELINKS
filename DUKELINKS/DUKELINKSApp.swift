c82ccb3b80593e9fe3d85bd3e130f16b357a462d
qihan-z <69850024+qihan-z@users.noreply.github.com>
2021-05-13 18:39:58 +0800

    Add files via upload

---
+++ DUKELINKS/DUKELINKSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DUKELINKSApp.swift
+//  DUKELINKS
+//
+//  Created by QiHan Zhao on 2021/5/13.
+//
+
+import SwiftUI
+
+@main
+struct DUKELINKSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
